aceddd3eb557ff37deeda77c84d13cbd72db8016
Davidson Sousa <dev@davidsonsousa.net>
2019-07-07 20:26:40 +0200

    Added ExpressionBuilder and Enums

---
+++ ReportsUsingDataTables/ReportsWeb/Enums.cs
@@ -0,0 +1,25 @@
+﻿namespace ReportsWeb
+{
+    public enum Operations
+    {
+        Coalesce,
+        Equals,
+        NotEqual,
+        GreaterThan,
+        LessThan,
+        GreaterThanOrEqual,
+        LessThanOrEqual,
+        Contains,
+        NotContain,
+        StartsWith,
+        EndsWith
+    }
+
+    public enum LogicalOperators
+    {
+        And,
+        AndAlso,
+        Or,
+        OrElse
+    }
+}
